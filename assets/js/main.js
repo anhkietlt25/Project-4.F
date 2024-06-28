5ad5b86e0afcc9f293e371ad32adf080bbe7b3b1
anhkietlt25 <anhkietchau06@gmail.com>
2024-06-28 23:57:44 +0700

    làm phần Work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,26 @@
 const listNav = document.querySelectorAll('.navbar__list .navbar__item');
 // console.log(listNav);
 
-listNav.forEach((item) => {
-    item.addEventListener('click', (e) => {
+// cach 1
+for( let i = 0; i < listNav.length; i++  ) {
+    // console.log(listNav[i]);
+    listNav[i].addEventListener('click', function(e) {
         e.preventDefault();
-        listNav.forEach((removeItem) => {
-            removeItem.classList.remove('navbar__link--active');
+        listNav.forEach((item) => {
+            item.classList.remove('navbar__link--active');
         });
-        // console.log(item);
-        item.classList.toggle('navbar__link--active');
+        this.classList.add('navbar__link--active');
     });
-});
+}
+
+// cach 2
+// listNav.forEach((item) => {
+//     item.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         listNav.forEach((removeItem) => {
+//             removeItem.classList.remove('navbar__link--active');
+//         });
+//         // console.log(item);
+//         item.classList.toggle('navbar__link--active');
+//     });
+// });
